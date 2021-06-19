e1684b0e229e9f724a2d4e394ecc5ea7fb22427d
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-06-19 11:08:04 -0400

    twillio

--- create/createInSMS.js
+++ create/createInSMS.js
@@ -1,21 +1,14 @@
 const twilio = require('twilio')
 const MessagingResponse = require("twilio/lib/twiml/MessagingResponse");
 
-function createInSMS(context, event, callback) {
+function createInSMS(req, res) {
 
+    const twiml = new MessagingResponse();
+    console.log(MessagingResponse.MessageAttributes)
+    twiml.message('OHO!!!!');
 
-        let twiml = new Twilio.twiml.MessagingResponse();
-        twiml.message(`${event.From}: ${event.Body}`, {
-            to: '+15197228119'
-        });
-        callback(null, twiml);
-
-    // const twiml = new MessagingResponse();
-    // console.log(twiml)
-    // twiml.message('OHO!!!!');
-    //
-    // res.writeHead(200, { 'Content-Type': 'text/xml' });
-    // res.end(twiml.toString());
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
 }
 
 module.exports = createInSMS;
