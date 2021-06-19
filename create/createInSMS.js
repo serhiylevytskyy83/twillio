930ba35f20c24c096df3c9e2f71b4db63274ec73
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-06-19 10:20:27 -0400

    twillio

--- create/createInSMS.js
+++ create/createInSMS.js
@@ -3,18 +3,9 @@ const MessagingResponse = require("twilio/lib/twiml/MessagingResponse");
 
 function createInSMS(req, res) {
 
-    // const from = req.body.from;
-    // const text = req.body.body;
-    // res.send(`
-    // <Response>
-    //     <Message>
-    //         Hello ${from} you said ${text}
-    //     </Message>
-    // </Response>
-    // `)
     const twiml = new MessagingResponse();
 
-    twiml.message(`MESSAGE SUCCESSFULLY SENT!!! ${req.from}`);
+    twiml.message('OHO!!!!');
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
