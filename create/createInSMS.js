6e898fe03939bafbf5645bbd46c5040e4e903e95
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-06-20 18:36:10 -0400

    twillio

--- create/createInSMS.js
+++ create/createInSMS.js
@@ -5,7 +5,7 @@ function createInSMS(req, res) {
 
     const twiml = new MessagingResponse();
     let from = req.body.From;
-    console.log(from)
+
     twiml.message(`OHO!!!! ${from}`);
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
