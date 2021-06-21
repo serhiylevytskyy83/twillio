87548f71204154e98f448f7e24c12a25ca02441e
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-06-20 20:08:05 -0400

    twillio

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const routes = require("./message/core/routes");
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const dbConnect = require('./modules/core/db');
+const dbConnect = require('./message/core/db');
 
 const PORT = +process.env.PORT || 5000;
 
-
+dbConnect();
 parseResponse(app);
 routes(app)
 function parseResponse(app) {
