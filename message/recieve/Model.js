f4f1dc3caadb456296d5274469c5f5cd2678867e
serhiylevytskyy83 <serhiylevytskyy83@gmail.com>
2021-06-20 23:14:33 -0400

    twillio

--- message/recieve/Model.js
+++ message/recieve/Model.js
@@ -6,7 +6,7 @@ const smsReceiveSchema = new Schema({
     from: {type: String, required: true},
     to: {type: String, required: true},
     body: {type: String, required: true},
-    // direction: {type: String, required: true,},
+    direction: {type: String, required: true,},
 
 });
 
